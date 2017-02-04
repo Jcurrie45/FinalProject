9b6aeedcb9c5d7325f132e748bf82170d59b56de
Jeremy Currie <jcurrie@student.neumont.edu>
2017-02-03 20:41:53 -0700

    for errlin <3

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
     var normalsize = $('#phonenumber').css('font-size');
     
+    var enlargedText = false;
     $("#phone").click(function(){
+        if(enlargedText){
+        normalsize = parseFloat(enlargesize)/1.5;
+            $('#phonenumber').css('font-size',normalText);
+        }
+        else{
         var enlargedsize = parseFloat(normalsize)*1.5;
         $('#phonenumber').css('font-size', enlargedsize);
+        }
     });
 });
