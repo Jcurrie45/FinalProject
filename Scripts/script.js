6fa046dc6fc0e929786d4959a07f8cb208392a15
Errlin Deleon <EDeleon@student.neumont.edu>
2017-02-03 22:01:46 -0700

    added a graph

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,3 +1,27 @@
+var Canvas = document.getElementById("myCanvas");
+var Context = Canvas.getContext('2d');
+
+function graph(){
+    Context.fillStyle = "#FFF";
+    Context.fillRect(0,0,1000,600);
+    
+    Context.beginPath();
+    Context.moveTo(0,600);
+    Context.lineTo(300,0);
+    Context.closePath();
+    Context.strokeStyle = '#00F';
+    Context.stroke();
+    
+    Context.beginPath();
+    Context.moveTo(0,600);
+    Context.lineTo(1000,0);
+    Context.closePath();
+    Context.strokeStyle = '#F00';
+    Context.stroke();
+}
+
+graph();
+
 $(document).ready(function(){
     var normalsize = $('#phonenumber').css('font-size');
     
