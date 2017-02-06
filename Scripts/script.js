27185d44f7532a3177d67b0af7f8b16ad0735f7c
Jeremy Currie <jcurrie@student.neumont.edu>
2017-02-06 09:54:06 -0700

    Errlin is ugly

--- Scripts/script.js
+++ Scripts/script.js
@@ -21,19 +21,3 @@ function graph(){
 }
 
 graph();
-
-$(document).ready(function(){
-    var normalsize = $('#phonenumber').css('font-size');
-    
-    var enlargedText = false;
-    $("#phone").click(function(){
-        if(enlargedText){
-        normalsize = parseFloat(enlargesize)/1.5;
-            $('#phonenumber').css('font-size',normalText);
-        }
-        else{
-        var enlargedsize = parseFloat(normalsize)*1.5;
-        $('#phonenumber').css('font-size', enlargedsize);
-        }
-    });
-});
---
+++ Scripts/script2.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    var normalsize = $('#phonenumber').css('font-size');
+    var notEnlarged = $('#address').css('font-size');
+    var enlargedText = false;
+    $("#phone").click(function(){
+        if(enlargedText){
+        normalsize = parseFloat(normalsize);
+            $('#phonenumber').css('font-size',normalsize);
+            enlargedText=false;
+        }
+        else if(!enlargedText){
+        var enlargedsize = parseFloat(normalsize)*1.5;
+        $('#phonenumber').css('font-size', enlargedsize);
+            enlargedText=true;
+        }
+    });
+enlargedText = false;
+    $("#address").click(function(){
+        if(enlargedText){
+        notEnlarged = parseFloat(notEnlarged);
+            $('#address').css('font-size',notEnlarged);
+            enlargedText=false;
+        }
+        else if(!enlargedText){
+        var enlargedsize = parseFloat(notEnlarged)*1.5;
+        $('#address').css('font-size', enlargedsize);
+            enlargedText=true;
+        }
+    });
+});
